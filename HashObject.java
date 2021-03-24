cde852a21ea7f319fe961fee75b0ff9ef1219dcb
Mariocise <sjhumrichouse@gmail.com>
2021-03-23 21:13:44 -0600

    Created HashObject object

--- HashObject.java
+++ HashObject.java
@@ -1,4 +1,38 @@
 
-public class HashObject {
+public class HashObject<T> {
 
+	T genericObject;
+	int duplicateCount;
+	int probeCount;
+	
+	T getObject()
+	{
+		return genericObject;
+	}
+	
+	void setObject(T newObject)
+	{
+		genericObject = newObject;
+	}
+	
+	int getDuplicateCount()
+	{
+		return duplicateCount;
+	}
+	
+	void incrementDuplicateCount()
+	{
+		duplicateCount++;
+	}
+	
+	int getProbeCount()
+	{
+		return probeCount;
+	}
+	
+	void incrementProbeCount()
+	{
+		probeCount++;
+	}
+	
 }
